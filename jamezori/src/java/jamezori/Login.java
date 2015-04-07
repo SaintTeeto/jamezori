ac05212fc5d6953963db8109bb5deca13eac71be
m_1997 <merlina.islami@hotmail.com>
2015-04-07 08:53:17 +0200

    Login-Seite

---
+++ jamezori/src/java/jamezori/Login.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jamezori;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author 5ia13meislami
+ */
+@ManagedBean
+@RequestScoped
+
+public class Login {
+
+    private String name;
+    private String password;
+
+    public String mainSide() {
+
+        return "mainSide.xhtml";
+    }
+
+    public String regSide() {
+
+        return "register.xhtml";
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
